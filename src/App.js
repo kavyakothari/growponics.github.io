7d27dd6122a8df334a591fef7a6c738bb981f88a
PrathyushaDiwakarla <32226370+PrathyushaDiwakarla@users.noreply.github.com>
2022-05-25 13:30:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 // The main JavaScript file where the routes are created for the application
 
+// When the user signs in with Google SignIn, the App.js createUserProfileDocument(userAuth) function checks to see if the Google login is available
+// in the firestore user collections. If not, a copy of the account with the required fields will be created. The user login information is 
+// retrieved from the firestore users collections and passed in the userRef in App.js.
+
 import React from 'react';
 
 import { Switch,Route,Redirect } from 'react-router-dom';
@@ -15,12 +19,16 @@ import CheckoutPage from "./pages/checkout/checkout.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { selectCurrentUser } from "./redux/user/user.selector";
 
+// Components are like functions that return HTML elements
+// Using extends for the component to access React.Component()
 class App extends React.Component{
+  // // The initial state is basically a null 
   unsubscribefromAuth = null;
 
   componentDidMount(){
     const {setCurrentUser} = this.props;
-
+    
+// Creating a User Profile Document method from firebase
     this.unsubscribefromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -40,6 +48,7 @@ class App extends React.Component{
     this.unsubscribefromAuth();
   }
 
+  // Function to render some UI when its path matches the current URL
   render(){
     return (
       <div>
@@ -64,10 +73,13 @@ class App extends React.Component{
   }
 }
 
+// createStructuredSelector is a utility function for a common pattern that occurs when using Reselect
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 })
 
+// The mapDispatchToProps function will be called with dispatch as first argument 
+// The code calls this.props.setCurrentUser
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
